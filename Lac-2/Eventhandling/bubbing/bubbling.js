95c9017f56310d4124733becb58ff5105bbd2b2c
Nishit <nishitjp@outlook.com>
2026-05-26 09:02:50 +0530

    Delete Lac-2 directory

--- Lac-2/Eventhandling/bubbing/bubbling.js
+++
@@ -1,7 +0,0 @@
-// Bubbling is the process of an event propagating from the innermost element to the outermost element. When an event occurs on an element, it first triggers any event handlers on that element, then it moves up to the parent element and triggers any event handlers there, and so on until it reaches the root of the document.
-
-let li = document.querySelectorAll('ul');
-
-document.addEventListener('click', function (e) {
-    e.target.classList.toggle("lt");
-})
